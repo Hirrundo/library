8ba0f6ca72485836a5746a55c5965051a2ab9927
liza <lizzkalizzka@inbox.ru>
2026-07-01 15:59:06 +0300

    Создает форму добавления книги, валидирует данные

--- src/pages/books/books.tsx
+++ src/pages/books/books.tsx
@@ -1,21 +1,45 @@
-import{ useState } from 'react'
+import{ useEffect, useState } from 'react'
 import BookList from "../../components/books/bookList"
 import './books.css'
 import { mockBooks } from "../../moks/books"
 import BookSearch from '../../components/books/bookSearch'
+import type { KeyboardEvent } from 'react';
+import AddBooksModal from '../../components/addBooks/addBooksModal'
 
 const BooksPage=()=>{
   const [searchQuery, setSearchQuery] = useState('');
   const filteredBooks=mockBooks.filter((book)=>
   book.title.toLowerCase().includes(searchQuery.toLowerCase()));
-  
+  const[showModal,setShowModal]=useState(false)
+    const closeHendler=()=>{
+      setShowModal(false)
+    }
+    useEffect(()=>{
+      const escHendler=(e:KeyboardEvent)=>{
+        if(e.key==='Escape'){
+          closeHendler()
+        }
+      }
+      if(showModal){
+      document.addEventListener('keydown',escHendler);
+    }
+      return()=>{
+         document.removeEventListener('keydown',escHendler);
+      }
+    },[showModal])
     return(
-      <>
+        <>
+      <div className='page-header'>
+          <div>
         <h1 className="page-title">Каталог книг</h1>
         <p className="page-subtitle">
           Всего книг: <strong>{mockBooks.length}</strong>
         </p>
-
+        </div>
+          <button className='btn btn-primary' id='add-reader-btn' onClick={()=>setShowModal(true)}>
+          + Добавить книгу
+        </button>
+        </div>
         {/* <!-- Toolbar --> */}
         <div className="page-toolbar">
           <BookSearch onSearch={setSearchQuery} />
@@ -25,7 +49,8 @@ const BooksPage=()=>{
             </span>
             )} </div>
         <BookList books={filteredBooks}/>
-    </>
+        {showModal&&<AddBooksModal hendelClick={closeHendler}/>}
+</>
     )
 }
 
