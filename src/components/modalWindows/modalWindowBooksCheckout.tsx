d27c475ae7fe48237d39e5385d8a68cfb3ff2e6f
Liza <lizzkalizzka@inbox.ru>
2026-08-12 18:31:40 +0300

    исправление опечатки

--- src/components/modalWindows/modalWindowBooksCheckout.tsx
+++ src/components/modalWindows/modalWindowBooksCheckout.tsx
@@ -8,7 +8,7 @@ function ModalCheckoutBook ({ onConfirm, onCancel }: ModalCheckoutBookProps) {
   return (
     <div className="modal-overlay">
       <div className="modal">
-        <h2>Принять книгу?</h2>
+        <h2>Выдать книгу?</h2>
 
         <div className="modal-buttons">
           <button onClick={onConfirm}>
